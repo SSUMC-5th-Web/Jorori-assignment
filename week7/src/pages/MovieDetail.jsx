dfe2440d3a5dd4aee599af10e093b24ac845f2cc
minhyuk2 <71816821+minhyuk2@users.noreply.github.com>
2023-11-15 01:04:10 +0900

    week7 really start

---
+++ week7/src/pages/MovieDetail.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import * as Content from "../components/Movie.style"
+import {useLocation,useParams} from 'react-router-dom'
+import { IMG_BASE_URL } from "../components/Movie";
+
+export default function MovieDetail() {
+  const{id} = useParams();
+  const{state} = useLocation();
+  console.log(id);
+  console.log(state);
+  
+  return <Content.PageContainer>
+        <Content.PosterFull>
+      <Content.PosterImg src={IMG_BASE_URL+ state.poster_path} alt='영화포스터'/>
+      <Content.PosterName>
+        {state.title}
+      </Content.PosterName>
+      <div>
+        {state.overview}
+      </div>
+      </Content.PosterFull>
+  </Content.PageContainer>;
+}
